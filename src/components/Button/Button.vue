00e4b02fb27feb28323dca6936b4c73d9418e20c
wajuejinajia <wajuejinajia@gmail.com>
2025-02-11 20:15:32 +0800

    feat: 初步完善Button组件的功能

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,38 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <button
+    class="vk-button"
+    :class="{
+        [`vk-button--${type}`]: type,
+        [`vk-button--${size}`]: size,
+        'is-plain': plain,
+        'is-round': round,
+        'is-circle': circle,
+        'is-disabled': disabled,
+    }"
+    :disabled="disabled"
+    >
+        <span>
+            <slot />
+        </span>
+    </button>
+</template>
+
+<script lang="ts">
+// 设置个人组件的个性化名称 需要单独写一个script来设置组件的属性
+import { defineComponent} from 'vue';
+
+export default defineComponent({
+    name: 'cjlButton'
+})
+</script>
+
+<script setup lang="ts">
+import type { ButtonProps } from './types';
+
+defineProps<ButtonProps>()
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Button/types.ts
@@ -0,0 +1,38 @@
+// import { PropType } from "vue";
+
+// button组件的类型和大小
+export type ButtonType = 'primary' | 'success' | 'warning' | 'danger' | 'info'
+export type ButtonSize = 'large' | 'small'
+
+// button组件对应的类型约束
+export interface ButtonProps {
+    type?: ButtonType;
+    size?: ButtonSize;
+    plain?: boolean;
+    round?: boolean;
+    circle?: boolean;
+    disabled?: boolean;
+}
+
+// 和上面表示方法一样，按照自己喜欢的方式定义类型约束
+// export const buttonProps = {
+//     type: {
+//         type: String as PropType<ButtonType>
+//     },
+//     size: {
+//         type: String as PropType<ButtonSize>
+//     },
+//     plain: {
+//         type: Boolean
+//     },
+//     round: {
+//         type: Boolean
+//     },
+//     circle: {
+//         type: Boolean
+//     },
+//     disabled: {
+//         type: Boolean
+//     },
+
+// }
