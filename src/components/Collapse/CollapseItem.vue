54971880b1a9da87b74813c62e1e12c258dc0395
wajuejinajia <wajuejinajia@gmail.com>
2025-02-19 16:23:06 +0800

    feat: 完成collapse组件的样式设置

--- src/components/Collapse/CollapseItem.vue
+++ src/components/Collapse/CollapseItem.vue
@@ -5,12 +5,24 @@
         'is-disabled': disabled
     }"
     >
-        <div class="vk-collapse-item__header" :id="`item-header-${name}`" @click="handleClick">
+        <div 
+            class="vk-collapse-item__header" 
+            :class="{
+                'is-disable': disabled,
+                'is-active': isActive
+            }"
+            :id="`item-header-${name}`" 
+            @click="handleClick"
+        >
             <slot name="title">{{ title }}</slot>
         </div>
-        <div class="vk-collapse-item__content" :id="`item-content-${name}`" v-show="isActive">
-            <slot />
-        </div>
+        <Transition name="slide" v-on="transitionEvents" >
+            <div class="vk-collapse-item__wrapper" v-show="isActive">
+                <div class="vk-collapse-item__content" :id="`item-content-${name}`">
+                    <slot />
+                </div>
+            </div>
+        </Transition>
     </div>
 </template>
 
@@ -28,6 +40,31 @@ const isActive = computed(() => collapseContext?.activeNames.value.includes(prop
 const handleClick = () => {
     if (props.disabled) {return}
     collapseContext?.handleItemClick(props.name)
+}
+
+const transitionEvents: Record<string, (el: HTMLElement) => void> = {
+    beforeEnter(el) {
+        el.style.height = '0px'
+        el.style.overflow = 'hidden'
+    },
+    enter(el) {
+        el.style.height = `${el.scrollHeight}`
+    },
+    afterEnter(el) {
+        el.style.height = ''
+        el.style.overflow = ''
+    },
+    beforeLeave(el) {
+        el.style.height = `${el.scrollHeight}`
+        el.style.overflow = 'hidden'
+    },
+    leave(el) {
+        el.style.height = '0px'
+    },
+    afterLeave(el) {
+        el.style.height = ''
+        el.style.overflow = ''
+    }
 }
 </script>
 
